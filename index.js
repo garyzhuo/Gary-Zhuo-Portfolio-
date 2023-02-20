cfeb142ee3562667f43a816823efd3e4d8891298
garyzhuo <gary_zhuo@hotmail.com>
2023-02-20 17:02:23 -0600

    made working carousel i think???

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+/* about me picture rotation 
+referred back to the corgi carousel */
+let currentImageIndex = 0;
+let previousImageIndex = 0;
+
+const images = document.getElementsByClassName('images');
+
+images[currentImageIndex].style.display = 'block';
+
+
+const next = document.querySelector('#aboutMeButton') 
+next.addEventListener('click', () => {
+    previousImageIndex = currentImageIndex
+    //using ++ makes it go to the next page
+    currentImageIndex++
+    images[previousImageIndex].style.display = 'none';
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    images[currentImageIndex].style.display = 'block'
+})
+
+const previous = document.querySelector('#previousImage')
+previous.addEventListener('click', () => {
+    previousImageIndex = currentImageIndex
+    // using -- makes it go back a image
+    currentImageIndex--
+    images[previousImageIndex].style.display = 'none'
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length -1;
+    }
+    images[currentImageIndex].style.display = 'block'
+})
+
+/*--------------------------------------------------------------------------------*/
+
+
+
